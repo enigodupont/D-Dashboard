2525ea124e9675b82be0eb7ceec67df6cdc27cf9
Juan Carlos Ramirez <jc6482@yahoo.com>
2018-01-14 22:41:57 -0600

    FINISHED THE BAGS AND INVENTORY

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,11 +1,13 @@
 class InventoryController < ApplicationController
+    include  UtilityHelper
 
     def BOH
-        if session[:user].nil? or session[:user]['username'].nil?
-            redirect_to '/users/login'
+        
+        if !checkLogin()
+            return
         end
-
-        @bohs = BagOfHolding.where(user_id: session[:user]['user_id'])
+        
+        @bohs = BagOfHolding.where(user_id: session[:user]['id'])
 
         if @bohs.nil?
             render "newBOH"
@@ -20,26 +22,118 @@ class InventoryController < ApplicationController
     end
 
     def showBOHs
+        if !checkLogin()
+            return
+        end
+
         if @bohs.nil?
             redirect_to root_url
         end       
     end
     
     def createBOH
-        @boh = BagOfHolding.new(user_params)
+        if !checkLogin()
+            return
+        end
+        @boh = BagOfHolding.new(bag_params.merge(:user_id => session[:user]['id']))
         if @boh.save
           redirect_to "/inventory/BOH"
         else
-          flash[:notice] = "Register Failed"
-          flash[:color] = "invalid"
+          flash[:notice] = "Bag Creation Failed"
+          flash[:color] = "text-danger"
           render "newBOH"
         end
     end
 
+    def deleteBOH
+        if !checkLogin()
+            return
+        end
+        
+        deletedBags = BagOfHolding.delete(params['id'])
+
+        if deletedBags < 1
+            flash[:notice] = "Bag Deletion Failed"
+            flash[:color] = "text-danger"
+        else
+            flash[:notice] = "Bag Deletion Complete"
+            flash[:color] = "text-success"
+        end
+
+
+        self.BOH
+        return
+    end
+
+    def showInventory
+        if !checkLogin()
+            return
+        end
+        if params[:id].nil?
+            redirect_to "/inventory/BOH"
+        end
+        
+        boh = BagOfHolding.where(user_id: session[:user]['id'], id: params[:id])
+
+        if boh.nil? or boh[0].nil?
+            flash[:notice] = "Could not find bag!"
+            flash[:color] = "text-danger"
+            self.BOH
+            return
+        end
+
+        @title = boh[0]['name']
+        @BOHID = boh[0]['id']
+        @inventory = Inventory.where(bag_id: boh[0]['id'])
+    end
+
+    def newItem
+        @item = Inventory.new
+    end
+        
+    def createItem
+        if !checkLogin()
+            return
+        end
+        @item = Inventory.new(item_params)
+        if @item.save
+            params['id'] = item_params['bag_id']
+            self.showInventory
+            render "showInventory"
+        else
+            flash[:notice] = "Item Failed"
+            flash[:color] = "text-danger"
+          render :action => "newItem", :bohid => params[:bohid]
+        end
+    end
+
+    def deleteItem
+        if !checkLogin()
+            return
+        end
+        
+        deletedBags = Inventory.delete(params['id'])
+
+        if deletedBags < 1
+            flash[:notice] = "Item Failed"
+            flash[:color] = "text-danger"
+        else
+            flash[:notice] = "Item Complete"
+            flash[:color] = "text-success"
+        end
+
+        #I should probably just make the params cleaner, but for now this is what it do
+        params['id'] = params['bag_id']
+        self.showInventory
+        render "showInventory"
+    end
 
   private
 
-  def user_params
+  def bag_params
     params.require(:bag_of_holding).permit(:name)
+  end
+  def item_params
+    params.require(:inventory).permit(:itemName, :weight, :bag_id)
   end
 end
