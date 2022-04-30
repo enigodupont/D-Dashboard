20ed42b1e876fbe93576b540caf81b61c5d774ea
Juan Carlos Ramirez <juramir@protonmail.com>
2022-04-29 21:19:53 -0500

    Update ruby gems, rails, and script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ echo "--------------------------------------------------------------------------
 if [ "${RAILS_ENVIRONMENT}" != "production" ] 
 then
   echo "Migrating for non production environment"
-  /opt/D-Dashboard/bin/rails db:migrate RAILS_ENV=${RAILS_ENVIRONMENT}
+  /opt/D-Dashboard/bin/rails db:migrate RAILS_ENV="${RAILS_ENVIRONMENT}"
 else
   echo "No migrate necessary"
 fi
 echo "--------------------------------------------------------------------------------"
 
-/opt/D-Dashboard/bin/rails s -e ${RAILS_ENVIRONMENT} -b 0.0.0.0
+/opt/D-Dashboard/bin/rails s -e "${RAILS_ENVIRONMENT}" -b 0.0.0.0
